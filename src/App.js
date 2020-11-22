5c9e4a163b9162c74ae0e06d787024c7e5d90c83
KazhymukhanYerkinov <61079138+KazhymukhanYerkinov@users.noreply.github.com>
2020-11-22 21:23:27 +0600

    'yes'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import Chat from './components/Chat/Chat';
 import Join from './components/Join/Join';
 
 
 const App = () => {
     return (
-        <Router>
+        <Router basename = {process.env.PUBLIC_URL}>
             <Route path = {'/'} exact component = { Join } />
             <Route path = {'/chat'} exact component = { Chat } />
 
